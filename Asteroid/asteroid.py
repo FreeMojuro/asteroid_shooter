5a747fa15232dbf7842fbf7d43ef89df549e964c
FreeMojuro <denizefemecan@gmail.com>
2026-06-28 18:17:52 +0300

    add asteroids project

---
+++ Asteroid/asteroid.py
@@ -0,0 +1,36 @@
+from circleshape import *
+from pygame import *
+from constants import *
+from logger import *
+import random
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        super().__init__(x, y, radius)
+    
+    def draw(self,screen):
+        pygame.draw.circle(screen,"white",self.position,self.radius,LINE_WIDTH)
+    
+    def update(self,dt: float) -> None:
+        self.position += self.velocity * dt
+        
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            x = random.uniform(20,50)
+            new_vector1 = self.velocity.rotate(x)
+            new_vector2= - self.velocity.rotate(x)
+            
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid1 = Asteroid(self.position.x,self.position.y,new_radius)
+            asteroid2 = Asteroid(self.position.x,self.position.y,new_radius)
+            asteroid1.velocity = new_vector1 * 1.2
+            asteroid2.velocity = new_vector2 * 1.2
+            
+            
+            
+            
